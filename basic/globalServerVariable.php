5f115377898ea7141f1bcd72372e692176996c11
tahir afridi <74824067+tahirafr999@users.noreply.github.com>
2022-04-16 23:32:10 +0500

    server information are added to the git

--- basic/globalServerVariable.php
+++ basic/globalServerVariable.php
@@ -10,4 +10,29 @@ echo "<br>";
 echo $_SERVER['HTTP_USER_AGENT'];
 echo "<br>";
 echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+echo $_SERVER['GATEWAY_INTERFACE'];
+echo "<br>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+echo $_SERVER['SERVER_PROTOCOL'];
+echo "<br>";
+echo $_SERVER['REQUEST_METHOD'];
+echo "<br>";
+echo $_SERVER['REQUEST_TIME'];
+echo "<br>";
+echo $_SERVER['QUERY_STRING'];
+echo "<br>";
+echo $_SERVER['SERVER_ADMIN'];
+echo "<br>";
+echo $_SERVER['SERVER_PORT'];
+echo "<br>";
+echo $_SERVER['SERVER_SIGNATURE'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";	
+
+// all task are done
 ?>
